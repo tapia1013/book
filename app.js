55e3d608062b9432508b249782c860a8a917d58b
tapia1013 <53988501+tapia1013@users.noreply.github.com>
2019-10-16 10:46:43 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -15,10 +15,59 @@ function UI() { }
 
 // create a prototype ADD BOOK TO LIST
 UI.prototype.addBookToList = function (book) {
-  console.log(book);
+  const list = document.getElementById('book-list')
+
+  // Create aT <tr></tr> element
+  const row = document.createElement('tr')
+
+  // INSERT COLS ... take <tr></tr> and append
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `
+
+  // APPEND TO SHOW
+  list.appendChild(row)
 
 }
 
+// SHOW ALERT
+UI.prototype.showAlert = function (message, className) {
+  // create div.... construct the element
+  const div = document.createElement('div')
+
+  // Add classes name
+  div.className = `alert ${className}`
+
+  // Add text with textnode
+  div.appendChild(document.createTextNode(message));
+
+  // Insert into dom...... GET PARENT
+  const container = document.querySelector('.container')
+
+  // Get form
+  const form = document.querySelector('#book-form');
+
+  // take parent container with insertBefore insert alert
+  container.insertBefore(div, form);
+
+  // disappear after 3 seconds aka timeOut
+  setTimeout(function () {
+    // grab alert class and remove it
+    document.querySelector('.alert').remove();
+  }, 3000)
+}
+
+
+// CLear Fields
+UI.prototype.clearFields = function () {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+
+}
 
 
 
@@ -39,12 +88,26 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
   //Instantiate a UI object
   const ui = new UI();
 
-  // Add book from new Book to html list
-  ui.addBookToList(book)
+  // Validate
+  if (title === '' || author === '' || isbn === '') {
+
+    // Error alert     v message               error class
+    ui.showAlert('Please fill in all fields', 'error');
+
+  } else {
+
+    // Add book from new Book to html list
+    ui.addBookToList(book);
 
 
+    // Show alert if success/ book added
+    ui.showAlert('Book Added!', 'success')
 
+    // Clear fields after adding to page with appendChild
+    ui.clearFields()
 
+  }
 
   e.preventDefault();
 })
+
